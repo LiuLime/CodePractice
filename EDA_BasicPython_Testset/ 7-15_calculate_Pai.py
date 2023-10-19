73eed81ce2688c74c5973b471f8abc43232114ac
LiuLime <hibarikyo505@gmail.com>
2023-10-19 23:41:31 +0900

    update 7-15.py

--- EDA_BasicPython_Testset/ 7-15_calculate_Pai.py
+++ EDA_BasicPython_Testset/ 7-15_calculate_Pai.py
@@ -1,32 +1,29 @@
 # 7-15  计算圆周率
 import math
 
-def an(n):
-    """return pai的值"""
-    # 实现阶乘的两种方式
-    # 1
-    # fenzi = math.factorial(n)
-    # 2
-    fenzi = 1
+
+def the_an_item(n):
+    """return an的值"""
+    fenzi = math.factorial(n)
     fenmu = 1
-    for i in range(n):
-        fenzi = (i+1)*fenzi
+
+    for i in range(1, n+1):
         fenmu = (2*i+1)*fenmu
+
     return fenzi/fenmu
 
 
 shrefold = float(input(""))
 
-
 if shrefold < 1:
-    n = 1
-    sum = 0
+    n = 0
+    an_sum = 0
     while True:
-        an = an(n)
+        an = the_an_item(n)
+        an_sum += an * 2
         if an > shrefold:
-            n+=1
-            sum += 2*an
+            n += 1
         else:
-            print("%.6f"%sum)
+            print("%.6f"%an_sum,end='')
             break
 
