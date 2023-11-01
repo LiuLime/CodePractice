e476759303a6f9990022822cde848949badfa233
LiuLime <liuyuting0219@gmail.com>
2023-11-01 16:07:26 +0900

    update distrbuted training example

--- Kaggle/rotate_tomato/sentimental_analysis/RNN_multigpu/main.py
+++ Kaggle/rotate_tomato/sentimental_analysis/RNN_multigpu/main.py
@@ -1,58 +1,104 @@
-'''
+"""
 The entry of the model
 
 copyright@ Liu
-'''
+"""
 
-import argparse
+
 import os, sys
 
 path = os.getcwd()
 sys.path.append(path)
+
 from model import BiRNN
 from parser import get_args
 import train
-import data_process
+import dataloader
 import prediction
 
 import torch
 import pandas as pd
-from transformers import AutoTokenizer
-from pathlib import Path
+import torch.optim as optim
+import torch.nn as nn
+from torch.distributed import destroy_process_group
+import torch.multiprocessing as mp
 
 
+def load_train_objs(args):
+    with open(os.path.join(args.data_path, args.train_file), "r") as t:
+        trainset = pd.read_csv(t, sep="\t", keep_default_na=False)
 
-def main():
+    token = dataloader.token(args.tokenizer)
+    train_iter, eval_iter = token.dataset_iter(
+        trainset, args.train_column, train=True, target_column=args.label_column
+    )
+    model = BiRNN(args)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss(reduction="mean")
 
+    return train_iter, eval_iter, model, optimizer, criterion
+
+
+def load_test_objs(args):
+    with open(os.path.join(args.data_path, args.test_file), "r") as t:
+        testset = pd.read_csv(t, sep="\t", keep_default_na=False)
+
+    token = dataloader.token(args.tokenizer)
+    test_iter = token.dataset_iter(testset, args.train_column, train=False)
+
+    # load from checkpoint
+    checkpoint = torch.load(args.model_path)
+    model = BiRNN(args)
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    return test_iter, model, testset[args.identifier_column]
+
+
+def main(
+    rank: int,
+    world_size: int,
+    args,
+):
+    if args.test == False:
+        train.ddp_setup(rank, world_size)
+        train_iter, eval_iter, model, optimizer, criterion = load_train_objs(args)
+        trainer = train.Train(
+            args,
+            train_iter,
+            eval_iter,
+            rank,
+            model,
+            optimizer,
+            criterion,
+        )
+        trainer.train()
+        destroy_process_group()
+
+    if args.test == True:
+        prediction.ddp_setup(rank, world_size)
+        test_iter, model, identifier = load_test_objs(args)
+        indices, labels = prediction.preds(
+            rank,
+            world_size,
+            test_iter,
+            model,
+        )
+        destroy_process_group()
+
+    if rank == 0 and labels is not None and indices is not None:
+        print(f"{len(labels)} prediction finished")
+        pred_reports = {
+            args.identifier_column: identifier,
+            args.label_column: labels,
+            "origin_index": indices,
+        }
+        pd.DataFrame(pred_reports).to_csv(
+            f"{args.output_path}/preds_reports.csv", index=False
+        )
+        print(f"prediction report saved at {args.output_path}")
+
+
+if __name__ == "__main__":
     args = get_args()
-
-    args.tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
-    args.vocab_size = args.tokenizer.vocab_size
-    args.world_size = torch.cuda.device_count()
-
-    # Load the datasets
-    token = data_process.tokenization(args.tokenizer)
-
-    # model = BiRNN(args).to(args.device)
-    if not args.test:
-        with open(os.path.join(args.data_path, args.train_file), 'r') as t:
-            trainset = pd.read_csv(t, sep='\t', keep_default_na=False)
-
-        train_iter, evaluate_iter = token.dataset_iter(trainset, args.content, train=True,
-                                                    target_column=args.label_column)
-        t = train.Train(args, train_iter, evaluate_iter)
-        t.BiRNN_train(model)
-    else:
-        with open(os.path.join(args.data_path, args.test_file), 'r') as t:
-            testset = pd.read_csv(t, sep='\t', keep_default_na=False)
-
-        model.load_state_dict(torch.load(args.model_path))
-        test_iter = token.dataset_iter(testset, args.content, train=False, target_column=args.label_column)
-        labels = prediction.Prediction.BiRNN_preds(model, test_iter, args.device)
-        pred_reports = {args.content: testset[args.content],
-                        args.label_column: labels}
-        pd.DataFrame(pred_reports).to_csv(f'{args.output_path}/preds_reports.csv', index=False)
-
-
-if __name__ == '__main__':
-    main()
+    # args.world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(args.world_size, args), nprocs=args.world_size)
--- Kaggle/rotate_tomato/sentimental_analysis/RNN_multigpu/parser.py
+++ Kaggle/rotate_tomato/sentimental_analysis/RNN_multigpu/parser.py
@@ -1,60 +1,159 @@
 import json
 import argparse
 from pathlib import Path
+from transformers import AutoTokenizer
+import torch
+
 
 def load_config(path):
-    with open (path, "r") as j:
+    with open(path, "r") as j:
         config = json.load(j)
     return config
 
 
 def get_args():
-
-    parent_path = Path(__name__).parent 
     config = load_config("config.json")
+    parent_path = Path(__name__).parent
 
     relative_data_path = Path(config["data_path"])
     relative_output_path = Path(config["output_path"])
-    relative_model_path =Path(config["model_path"])
+    relative_model_path = Path(config["model_path"])
 
     parser = argparse.ArgumentParser(description="distributed training job for BiRNN")
 
     # Path and file configs
-    parser.add_argument('--data_path', default=relative_data_path, help='The dataset path.', type=str)
-    parser.add_argument('--model_path', default=relative_model_path, help='The model will be saved to this path.',
-                        type=str)
-    parser.add_argument('--output_path', default=relative_output_path,
-                        help='The predictions will be saved to this path.',
-                        type=str)
-    parser.add_argument('--train_file', default=config["train_file"], help='default=train.tsv',type=str)
-    parser.add_argument('--val_file', default=None, help='validation file, default=None',type=str)
-    parser.add_argument('--test_file', default=config["test_file"], help='default=test.tsv', type=str)
-    parser.add_argument('--content', default=config["content"], help='The content column, default=Phrase', type=str)
-    parser.add_argument('--label_column', default=config["label_column"], help='The label column, default=labels', type=str)
+    parser.add_argument(
+        "--data_path", default=relative_data_path, help="The dataset path.", type=str
+    )
+    parser.add_argument(
+        "--model_path",
+        default=relative_model_path,
+        help="The model will be saved to this path.",
+        type=str,
+    )
+    parser.add_argument(
+        "--output_path",
+        default=relative_output_path,
+        help="The predictions will be saved to this path.",
+        type=str,
+    )
+    parser.add_argument(
+        "--train_file", default=config["train_file"], help="default=train.tsv", type=str
+    )
+    parser.add_argument(
+        "--val_file", default=None, help="validation file, default=None", type=str
+    )
+    parser.add_argument(
+        "--test_file", default=config["test_file"], help="default=test.tsv", type=str
+    )
+    parser.add_argument(
+        "--train_column",
+        default=config["train_column"],
+        help="The train column, default=Phrase",
+        type=str,
+    )
+    parser.add_argument(
+        "--label_column",
+        default=config["label_column"],
+        help="The label column, default=labels",
+        type=str,
+    )
+    parser.add_argument(
+        "--identifier_column",
+        default=config["identifier_column"],
+        help="The identifier column used for prediction file, default=PhraseId",
+        type=str,
+    )
 
     # Model configs
-    parser.add_argument('--tokenizer_name', default=config["tokenizer_name"],
-                        help='pre-trained tokenizer, default=distilbert-base-uncased', type=str)
+    parser.add_argument(
+        "--tokenizer",
+        default=config["tokenizer"],
+        help="pre-trained tokenizer, default=distilbert-base-uncased",
+        type=str,
+    )
 
-    parser.add_argument('--embedding_dim', default=config["embedding_dim"], help='Tokens will be embedded to a vector, default=128', type=int)
-    parser.add_argument('--hidden_dim', default=config["hidden_dim"], help='The hidden state dim of BiLSTM. default=256', type=int)
-    parser.add_argument('--output_dim', default=config["output_dim"], help='output dim of BiLSTM-> num of label class, default=5', type=int)
-    parser.add_argument('--num_layers', default=config["num_layers"], help='number of LSTM layers, default=2', type=int)
+    parser.add_argument(
+        "--embedding_dim",
+        default=config["embedding_dim"],
+        help="Tokens will be embedded to a vector, default=128",
+        type=int,
+    )
+    parser.add_argument(
+        "--hidden_dim",
+        default=config["hidden_dim"],
+        help="The hidden state dim of BiLSTM. default=256",
+        type=int,
+    )
+    parser.add_argument(
+        "--output_dim",
+        default=config["output_dim"],
+        help="output dim of BiLSTM-> num of label class, default=5",
+        type=int,
+    )
+    parser.add_argument(
+        "--num_layers",
+        default=config["num_layers"],
+        help="number of LSTM layers, default=2",
+        type=int,
+    )
 
     # Optimizer config
-    parser.add_argument('--lr', default=config["lr"], help='Learning rate of the optimizer,default=1e-4', type=float)
+    parser.add_argument(
+        "--lr",
+        default=config["lr"],
+        help="Learning rate of the optimizer,default=1e-4",
+        type=float,
+    )
 
     # Training configs
-    parser.add_argument('--train_batch_size', default=config["train_batch_size"], help='training batch size, default=64', type=int)
-    parser.add_argument('--test_batch_size', default=config["test_batch_size"], help='test batch size, default=64', type=int)
-    parser.add_argument('--num_epochs', default=config["num_epochs"], help='training epochs number, default=10.', type=int)
-
-    # Device config
+    parser.add_argument(
+        "--train_batch_size",
+        default=config["train_batch_size"],
+        help="training batch size, default=64",
+        type=int,
+    )
+    parser.add_argument(
+        "--test_batch_size",
+        default=config["test_batch_size"],
+        help="test batch size, default=64",
+        type=int,
+    )
+    parser.add_argument(
+        "--num_epochs",
+        default=config["num_epochs"],
+        help="training epochs number, default=10.",
+        type=int,
+    )
+    parser.add_argument(
+        "--save_every_n_epoch",
+        default=config["save_every_n_epoch"],
+        help="checkpoint saved every n epoches, default=5",
+        type=int,
+    )
+    parser.add_argument(
+        "--world_size",
+        default=config["world_size"],
+        help="number of processes started, default=all gpu",
+        type=int,
+    )
 
     # Mode config
-    parser.add_argument('--test', default=config["test"],help='Test on the testset.', action='store_true')
-    parser.add_argument('--run_from_ckp', default=config["run_from_ckp"],help='rerun from checkpoint', action='store_true')
+    parser.add_argument(
+        "--test",
+        default=config["test"],
+        help="Test on the testset.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--run_from_ckp",
+        default=config["run_from_ckp"],
+        help="rerun from checkpoint",
+        action="store_true",
+    )
 
     args = parser.parse_args()
+    args.tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    args.vocab_size = args.tokenizer.vocab_size
 
     return args
